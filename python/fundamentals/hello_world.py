c7635b66cdb011c2ee9a70f1435b4761e995aa33
Alexis GF <alexisgfly16@hotmail.com>
2021-06-10 00:13:38 -0500

    Se modifica y se realiza tarea Hola Mundo

--- python/fundamentals/hello_world.py
+++ python/fundamentals/hello_world.py
@@ -1,5 +1,18 @@
-print("hello World!")
-x = "Hola Python"
-print(x)
-y = 42
-print(y)
+# TAREA HELLO WORLD
+
+# 1. TAREA: imprimir "Hola mundo"
+print("Hola Mundo")
+
+# 2. imprimir "Hola Noelle!" con el nombre en una variable
+name = "Alexis"
+print("Hola",name)	# con una coma
+print("Hola "+name)	# con un +
+# 3. imprimir "Hola 42!" con un numero en una variable
+name = "7"
+print("Hola",name )	# con una coma
+print("Hola "+name )	# con un + - !Este debería darnos un error!
+# 4. imprimir "Me encanta comer sushi and pizza." con los alimentos en variables
+fave_food1 = "alitas"
+fave_food2 = "pizza"
+print("Me encanta comer {} y también {}".format(fave_food1,fave_food2) ) # con .format()
+print(f"Me encanta comer {fave_food1} y también {fave_food2}") # con una cadena f
