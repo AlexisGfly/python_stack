1545da63b7fc2a23e27a9b9f6c0ea242b893d042
Alexis GF <alexisgfly16@hotmail.com>
2021-06-13 14:50:21 -0500

    Se realiza la tarea Chaining_methods.py

--- python/fundamentals/Chaining_methods.py
+++ python/fundamentals/Chaining_methods.py
@@ -5,11 +5,13 @@ class User:		# declara una clase y dale el nombre User
         self.account_balance = 0
 
     # agrega el método deposit 
-    def make_deposit(self, amount):	# toma un argumento que es el monto del depósito
-    	 self.account_balance += amount	# la cuenta del usuario específico aumenta por la cantidad del valor recibido
+    def make_deposit(self, amount):
+        self.account_balance += amount	# la cuenta del usuario específico aumenta por la cantidad del valor recibido
+        return self
     
     def make_withdrawal (self, amount): # toma un argumento que es el monto del débito
         self.account_balance -= amount	# la cuenta del usuario específico disminuye  la cantidad del valor recibido
+        return self
 
     def display_user_balance (self): # imprime el nombre del usuario y el saldo de la cuenta en el terminal
         print('\nUsuario:',self.name,', Saldo:',self.account_balance)
@@ -17,6 +19,7 @@ class User:		# declara una clase y dale el nombre User
     def transfer_money (self, other_user, amount):
         self.account_balance -= amount # Realiza el débito de la cuenta que transfiere
         other_user.make_deposit(amount) # Realiza la acreditación del saldo a la cuenta que recibe
+        return self
         
 # Se ingresan usuarios
 guido = User("Guido van Rossum", "guido@python.com")
@@ -28,41 +31,10 @@ alexis = User("Alexis Guamán", "alexis@python.com")
 
 # **************  Transacciones realizadas  ************************
 # ===> Usuario 1
-# Depósitos
-guido.make_deposit(100) # Depósito 1
-guido.make_deposit(200) # Depósito 2
-guido.make_deposit(50)  # Depósito 2
-# Retiros
-guido.make_withdrawal(120) # Retiro 1
-# Muestra saldo
-guido.display_user_balance() # Saldo actual
-
-
+guido.make_deposit(100).make_deposit(200).make_deposit(50).make_withdrawal(120).transfer_money(alexis,100).display_user_balance()
 # ===> Usuario 2
-# Depósitos
-monty.make_deposit(50)  # Depósito 1
-monty.make_deposit(150) # Depósito 2
-# Retiros
-monty.make_withdrawal(10) # Retiro 1
-monty.make_withdrawal(40) # Retiro 2
-# Muestra saldo
-monty.display_user_balance()
-
-
+monty.make_deposit(50).make_deposit(150).make_withdrawal(10).make_withdrawal(40).display_user_balance()
 # ===> Usuario 3
-# Depósitos
-alexis.make_deposit(500)  # Depósito 1
-# Retiros
-alexis.make_withdrawal(100) # Retiro 1
-alexis.make_withdrawal(50)  # Retiro 2
-alexis.make_withdrawal(90)  # Retiro 3
-# Muestra saldo
-alexis.display_user_balance()
+alexis.make_deposit(500).make_withdrawal(100).make_withdrawal(50).make_withdrawal(90).display_user_balance()
 
-# ===> Transferencias del Usuario 1 al Usuario 3
-# Transferencia realizada
-guido.transfer_money(alexis,100)
-# Imprime saldos
-print('\nSaldo posterior a la transferencia interbancaria')
-guido.display_user_balance()
-alexis.display_user_balance()
+
