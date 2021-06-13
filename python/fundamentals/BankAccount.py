baa87bb8bde88be4a2547f175052d068de509438
Alexis GF <alexisgfly16@hotmail.com>
2021-06-13 15:17:20 -0500

    Se realiza la tarea BankAccount.py

--- python/fundamentals/BankAccount.py
+++ python/fundamentals/BankAccount.py
@@ -1,12 +1,24 @@
 class BankAccount:
-	def __init__(self, int_rate, balance): # ¡No olvides agregar valores predeterminados para estos parámetros!
-		 # su código aquí! (recuerde, aquí es donde especificamos los atributos para nuestra clase)
-                # no se preocupe por la información del usuario aquí; pronto involucraremos a la clase de usuario
-	def deposit(self, amount):
-		# tu código aqui
-	def withdraw(self, amount):
-		# tu código aqui
-	def display_account_info(self):
-		# tu código aqui
-	def yield_interest(self):
-		# tu código aqui
+    def __init__(self, int_rate, balance):
+        self.rate = int_rate
+        self.account_balance = balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self
+    def withdraw(self, amount):
+        self.account_balance -= amount
+        return self
+    def display_account_info(self):
+        print('\tSaldo:',self.account_balance)
+
+    def yield_interest(self):
+        if self.account_balance > 0:
+            self.account_balance = self.account_balance * self.rate + self.account_balance
+            return self
+
+guido = BankAccount(0.01, 500)
+alexis = BankAccount(0.01, 600)
+
+guido.deposit(100).deposit(200).deposit(300).withdraw(500).yield_interest().display_account_info()
+alexis.deposit(250).deposit(250).withdraw(100).withdraw(200).withdraw(300).withdraw(400).yield_interest().display_account_info()
