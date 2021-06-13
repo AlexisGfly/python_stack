88e16090cdb8854d3efd3dee3516db628450ba6c
Alexis GF <alexisgfly16@hotmail.com>
2021-06-13 15:01:26 -0500

    Se crea el archivo BankAccount.py

---
+++ python/fundamentals/BankAccount.py
@@ -0,0 +1,12 @@
+class BankAccount:
+	def __init__(self, int_rate, balance): # ¡No olvides agregar valores predeterminados para estos parámetros!
+		 # su código aquí! (recuerde, aquí es donde especificamos los atributos para nuestra clase)
+                # no se preocupe por la información del usuario aquí; pronto involucraremos a la clase de usuario
+	def deposit(self, amount):
+		# tu código aqui
+	def withdraw(self, amount):
+		# tu código aqui
+	def display_account_info(self):
+		# tu código aqui
+	def yield_interest(self):
+		# tu código aqui
